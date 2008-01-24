54a38522906a81a0c755c13b130e65de0377db67
mnachev <mnachev@7201ef80-233e-0410-912c-3706d87a3af7>
2008-01-24 17:35:22 +0000

    The main Acacia Business Ace project. Use NetBeans IDE 6.0 to open it.

---
+++ trunk/AcaciaBusinessAce/AcaciaBusinessLogic/src/java/empty/EmptyEJBLocal.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package empty;
+
+import javax.ejb.Local;
+
+/**
+ *
+ * @author Miro
+ */
+@Local
+public interface EmptyEJBLocal {
+    
+}
