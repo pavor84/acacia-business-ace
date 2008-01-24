a28fce64d231cc09d19f7ed0817459b6fcb3c18a
mnachev <mnachev@7201ef80-233e-0410-912c-3706d87a3af7>
2008-01-24 19:07:52 +0000

    The main project of Acacia Business Ace. Use NetBeans IDE v6.0 or higher to open it.

---
+++ trunk/AcaciaBusinessAce/AcaciaBusinessLogic/src/java/empty/EmptyEJBBean.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package empty;
+
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author Miro
+ */
+@Stateless
+public class EmptyEJBBean implements EmptyEJBLocal {
+
+    public void voidMethod() {
+    }
+    
+    // Add business logic below. (Right-click in editor and choose
+    // "EJB Methods > Add Business Method" or "Web Service > Add Operation")
+ 
+}
