8f0ad663cbbb4f82ba6d72f4c82a08957d8d4fb1
mnachev <mnachev@7201ef80-233e-0410-912c-3706d87a3af7>
2008-01-27 15:59:05 +0000

    SwingB project replace all Swing and SwingX classes. This can help in the future if we need to do some specific requirement which is not possible with the existing Swing or SwingX libraries.

--- AcaciaBusinessAce/AcaciaBusinessAce-ejb/src/java/com/cosmos/acacia/crm/bl/impl/ProductSessionBean.java
+++ AcaciaBusinessAce/AcaciaBusinessAce-ejb/src/java/com/cosmos/acacia/crm/bl/impl/ProductSessionBean.java
@@ -6,11 +6,22 @@
 package com.cosmos.acacia.crm.bl.impl;
 
 import com.cosmos.acacia.crm.data.test.Product;
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.swing.JTable;
+import org.jdesktop.beansbinding.AutoBinding;
+import org.jdesktop.beansbinding.ELProperty;
+import org.jdesktop.swingbinding.JTableBinding;
+import org.jdesktop.swingbinding.JTableBinding.ColumnBinding;
+import org.jdesktop.swingbinding.SwingBindings;
 
 /**
  *
@@ -23,18 +34,108 @@ public class ProductSessionBean implements ProductSessionRemote, ProductSessionL
     @PersistenceContext
     private EntityManager em;
 
+    public void beginTransaction()
+    {
+        em.getTransaction().begin();
+    }
+
+    public void commitTransaction()
+    {
+        em.getTransaction().begin();
+    }
+
+    public void rollbackTransaction()
+    {
+        em.getTransaction().rollback();
+    }
+
+    public EntityTransaction getTransaction()
+    {
+        return em.getTransaction();
+    }
+
+    @PostConstruct
+    public void init()
+    {
+        System.out.println("init()");
+    }
+
+    @PreDestroy
+    public void destroy()
+    {
+        EntityTransaction tx = getTransaction();
+        System.out.println("tx.isActive(): " + tx.isActive());
+        if(tx.isActive())
+            tx.rollback();
+        System.out.println("destroy()");
+    }
+
     public List<Product> getProducts() {
         System.out.println("em: " + em);
         if(em != null)
         {
-            List data = em.createQuery("select p from Product p").getResultList();
-            if(data != null)
-                System.out.println("data.className: " + data.getClass().getName());
-            System.out.println("data: " + data);
-            return new ArrayList<Product>(data);
+            //beginTransaction();
+            try
+            {
+                Query q = em.createQuery("select p from Product p");
+                List data = q.getResultList();
+                if(data != null)
+                {
+                    System.out.println("data.className: " + data.getClass().getName());
+                    if(data.isEmpty())
+                    {
+                        System.out.println("beginTransaction()");
+                        Product product = new Product(new BigDecimal(1), "1st Product");
+                        em.persist(product);
+                        product = new Product(new BigDecimal(2), "2nd Product");
+                        em.persist(product);
+                        product = new Product(new BigDecimal(3), "3rd Product");
+                        em.persist(product);
+                        System.out.println("commitTransaction()");
+                        //commitTransaction();
+
+                        //beginTransaction();
+                        System.out.println("q.getResultList()");
+                        data = q.getResultList();
+                    }
+                }
+
+                System.out.println("data: " + data);
+                return new ArrayList<Product>(data);
+            }
+            catch(Exception ex)
+            {
+                System.out.println("rollbackTransaction()");
+                //rollbackTransaction();
+                ex.printStackTrace();
+            }
         }
+
         return null;
     }
+
+    public JTableBinding getProductTableBinding(JTable targetTable) {
+        AutoBinding.UpdateStrategy updateStrategy = AutoBinding.UpdateStrategy.READ_WRITE;
+        JTableBinding tableBinding = SwingBindings.createJTableBinding(updateStrategy, getProducts(), targetTable);
+
+        // Determine the access for the Table depending on user, role, current object and hidden columns
+        // Restore the columns state depending on the previous session (visible, size, position)
+
+        ColumnBinding columnBinding = tableBinding.addColumnBinding(ELProperty.create("${productId}"));
+        columnBinding.setColumnName("Product Id");
+        columnBinding.setColumnClass(BigDecimal.class);
+        columnBinding.setEditable(false);
+        //columnBinding.setVisible(false);
+
+        columnBinding = tableBinding.addColumnBinding(ELProperty.create("${productName}"));
+        columnBinding.setColumnName("Product Name");
+        columnBinding.setColumnClass(String.class);
+        columnBinding.setEditable(true);
+        //columnBinding.setVisible(true);
+
+        return tableBinding;
+    }
+
 
     //public void persist(Object object) {
     //    em.persist(object);
