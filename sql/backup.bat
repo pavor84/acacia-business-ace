f966e447ca6751ad43d30736a68a180d1d2744c9
mnachev <mnachev@7201ef80-233e-0410-912c-3706d87a3af7>
2008-02-19 19:03:25 +0000

    sql backup script

--- sql/backup.bat
+++ sql/backup.bat
@@ -1,2 +1,2 @@
 set pgsql_path=C:\Program Files (x86)\PostgreSQL\8.3\bin
-%pgsql_path%\pg_dump -U PostgreSQL -ci -F t -f acacia-ddl-%1.tar acacia
+%pgsql_path%\pg_dump -i -h localhost -p 5432 -U PostgreSQL -F p -O -D -v -f acacia-%1.sql acacia
