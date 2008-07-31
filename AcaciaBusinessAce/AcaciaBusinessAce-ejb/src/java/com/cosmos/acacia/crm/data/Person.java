5937afec42cc44177852ee39d50f1cdb0d36c209
bozhidar.bozhanov <bozhidar.bozhanov@7201ef80-233e-0410-912c-3706d87a3af7>
2008-07-31 21:44:14 +0000

    Adding newly registered user as contact person to the chosen branch

--- AcaciaBusinessAce/AcaciaBusinessAce-ejb/src/java/com/cosmos/acacia/crm/data/Person.java
+++ AcaciaBusinessAce/AcaciaBusinessAce-ejb/src/java/com/cosmos/acacia/crm/data/Person.java
@@ -64,22 +64,22 @@ public class Person extends BusinessPartner
 
     @Column(name = "first_name", nullable = false)
     @Property(title="First Name",
-            propertyValidator=@PropertyValidator(validationType=ValidationType.LENGTH, minLength=1, maxLength=255))
+            propertyValidator=@PropertyValidator(validationType=ValidationType.LENGTH, minLength=1, maxLength=24))
     private String firstName;
 
     @Column(name = "second_name")
     @Property(title="Second Name",
-        propertyValidator=@PropertyValidator(validationType=ValidationType.LENGTH, minLength=1, maxLength=255))
+        propertyValidator=@PropertyValidator(validationType=ValidationType.LENGTH, minLength=1, maxLength=24))
     private String secondName;
 
     @Column(name = "last_name", nullable = false)
     @Property(title="Last Name",
-        propertyValidator=@PropertyValidator(validationType=ValidationType.LENGTH, minLength=1, maxLength=255))
+        propertyValidator=@PropertyValidator(validationType=ValidationType.LENGTH, minLength=1, maxLength=24))
     private String lastName;
 
     @Column(name = "extra_name")
     @Property(title="Extra Name",
-        propertyValidator=@PropertyValidator(validationType=ValidationType.LENGTH, minLength=1, maxLength=255))
+        propertyValidator=@PropertyValidator(validationType=ValidationType.LENGTH, minLength=1, maxLength=24))
     private String extraName;
 
     @Column(name = "personal_unique_id")
