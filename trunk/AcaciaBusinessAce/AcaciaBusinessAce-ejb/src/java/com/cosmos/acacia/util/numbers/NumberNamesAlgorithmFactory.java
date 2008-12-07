dfd716221c093811901c230dda9e66eae738f629
bozhidar.bozhanov <bozhidar.bozhanov@7201ef80-233e-0410-912c-3706d87a3af7>
2008-12-07 19:01:24 +0000

    Moved printing entirely on the server

--- trunk/AcaciaBusinessAce/AcaciaBusinessAce-ejb/src/java/com/cosmos/acacia/util/numbers/NumberNamesAlgorithmFactory.java
+++ trunk/AcaciaBusinessAce/AcaciaBusinessAce-ejb/src/java/com/cosmos/acacia/util/numbers/NumberNamesAlgorithmFactory.java
@@ -2,21 +2,27 @@ package com.cosmos.acacia.util.numbers;
 
 import java.util.Locale;
 
+import org.jdesktop.application.ResourceMap;
+
 import com.cosmos.acacia.crm.enums.Currency;
 
 public class NumberNamesAlgorithmFactory {
 
-    public static NumberNamesAlgorithm getNumberNamesAlgorithm(Locale locale, NumberNamesAlgorithmType type)
+    public static NumberNamesAlgorithm getNumberNamesAlgorithm(Locale locale,
+            NumberNamesAlgorithmType type,
+            ResourceMap map)
         throws UnsupportedNumberAlgorithmException {
 
-        return getNumberNamesAlgorithm(locale, type, null);
+        return getNumberNamesAlgorithm(locale, type, null, map);
     }
-
-    public static NumberNamesAlgorithm getNumberNamesAlgorithm(Locale locale,
-            NumberNamesAlgorithmType type, Enum<Currency> enumValue) throws UnsupportedNumberAlgorithmException {
+    public static NumberNamesAlgorithm getNumberNamesAlgorithm(
+            Locale locale,
+            NumberNamesAlgorithmType type,
+            Enum<Currency> enumValue,
+            ResourceMap map) throws UnsupportedNumberAlgorithmException {
 
         if (locale.equals(new Locale("bg")) && type == NumberNamesAlgorithmType.MONEY)
-            return new BulgarianNumberNamesMoneyAlgorithm(enumValue);
+            return new BulgarianNumberNamesMoneyAlgorithm(enumValue, map);
 
         throw new UnsupportedNumberAlgorithmException(locale, type);
     }
--- trunk/AcaciaBusinessAce/AcaciaBusinessAce-ejb/src/java/com/cosmos/acacia/util/numbers/NumberNamesUtils.java
+++ trunk/AcaciaBusinessAce/AcaciaBusinessAce-ejb/src/java/com/cosmos/acacia/util/numbers/NumberNamesUtils.java
@@ -3,12 +3,6 @@ package com.cosmos.acacia.util.numbers;
 import java.math.BigDecimal;
 import java.math.BigInteger;
 
-import org.jdesktop.application.Application;
-import org.jdesktop.application.ResourceMap;
-
-import com.cosmos.acacia.crm.enums.Currency;
-import com.cosmos.acacia.crm.gui.AcaciaApplication;
-
 public class NumberNamesUtils {
 
     public static BigInteger getWholePart(BigDecimal decimal) {
@@ -19,10 +13,5 @@ public class NumberNamesUtils {
         return decimal.subtract(
                 new BigDecimal(decimal.toBigInteger()))
                 .movePointRight(2).toBigInteger();
-    }
-
-    public static ResourceMap getResourceMap() {
-        return Application.getInstance(AcaciaApplication.class)
-            .getContext().getResourceMap(Currency.class);
     }
 }
